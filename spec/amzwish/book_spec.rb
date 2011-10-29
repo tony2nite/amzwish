cf3d721b0e4afd657b0300405d602f276d2d316a
Anthony Hughes <tony2nite@gmail.com>
2011-10-29 12:47:50 +0100

    Add tests for book price comparision

--- spec/amzwish/book_spec.rb
+++ spec/amzwish/book_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 module Amzwish
   describe Book do
     describe "equality" do
-      let(:fixture){ Book.new("123", "Title") }
+      let(:fixture){ Book.new("123", "Title", 50) }
       describe "is based on asin number" do
         example "books with the same asin number are equal" do
           (fixture == Book.new("123", "Title")).should == true
@@ -14,6 +14,16 @@ module Amzwish
         end
         example "books are not equal to things that are not books" do
           (fixture == "Title").should == false
+        end
+        example "books are sorted by price" do
+          high = Book.new("456", "Title", 100)
+          (high <=> fixture).should == 1
+          
+          low = Book.new("789", "Title", 25)
+          (low <=> fixture).should == -1
+
+          same = Book.new("101112", "Title", 50)
+          (same <=> fixture).should == 0
         end
       end
     end
